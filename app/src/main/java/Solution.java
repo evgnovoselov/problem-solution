6397cea606ab1b368adbd7da7f69e694affa6917
Evgeny Novoselov <enovoselov@yahoo.com>
2024-02-27 14:15:29 +0300

    Сделал ввод данных

--- app/src/main/java/Solution.java
+++ app/src/main/java/Solution.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int echo = scanner.nextInt();
+        System.out.println(echo);
     }
 }
